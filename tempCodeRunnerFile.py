257dbdf26db33b6120662f17cefd3e66f4ba8fb2
Dylan Coxshall <63456774+DCoxshall@users.noreply.github.com>
2021-12-14 21:43:45 +0000

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-if (len([x for x in marking_boards if x != "deletedBoard"]) == 1) and not found_last_board:
-            final_board_index = j
-            final_board = [
-                i for i in marking_boards if i != "deletedBoard"][0]
-            found_last_board = True
